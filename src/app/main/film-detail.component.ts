a577a70886e513e39c534ec3ae52da634aa57db8
baransel.dinc <baransel.dinc@fibabanka.com.tr>
2022-11-07 23:56:17 +0300

    Fix delete film

--- src/app/main/film-detail.component.ts
+++ src/app/main/film-detail.component.ts
@@ -1,39 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, UrlSegment } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { data, film } from '../model/model';
 
 @Component({
-    selector: 'film-detail',
-    templateUrl: 'film-detail.component.html',
-    styleUrls: ['./film-detail.component.css']
+  selector: 'film-detail',
+  templateUrl: 'film-detail.component.html',
+  styleUrls: ['./film-detail.component.css'],
 })
-
 export class FilmDetailComponent implements OnInit {
-    filmId: string | null;
-    film: film | undefined;
-    categoryId: string | null;
+  filmId: string | null;
+  film: film | undefined;
+  categoryId: string | null;
 
-    constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private router: Router) {}
 
-    handleClickDeleteFilm() {
-        if (confirm(`${this.film?.filmName} isimli filmi silmek istediğinize emin misiniz?`)) {
-            const cat = data.find(cat => cat.categoryId === this.categoryId);
-            const index = cat?.list.findIndex(film => film.filmId === this.filmId);
-            if (index)
-                cat?.list.splice(index, 1);
-        }
+  handleClickDeleteFilm() {
+    if (
+      confirm(
+        `${this.film?.filmName} isimli filmi silmek istediğinize emin misiniz?`
+      )
+    ) {
+      const cat = data.find((cat) => cat.categoryId === this.categoryId);
+      const index = cat?.list?.findIndex((film) => film.filmId === this.filmId);
+      if (index !== undefined && index > -1) {
+        cat?.list.splice(index, 1);
+        this.router.navigate(['category', this.categoryId]);
+      }
     }
+  }
 
-    ngOnInit() {
-        this.route.parent?.paramMap.subscribe(paramMap => {
-            this.categoryId = paramMap.get('id');
-        })
+  ngOnInit() {
+    this.route.parent?.paramMap.subscribe((paramMap) => {
+      this.categoryId = paramMap.get('id');
+    });
 
-        this.route.paramMap.subscribe(paramMap => {
-            this.filmId = paramMap.get('id');
-            const category = data.find(cat => cat.categoryId === this.categoryId);
-            this.film = category?.list.find(film => film.filmId === this.filmId);
-            window.scrollTo(0, document.body.scrollHeight);
-        });
-    }
+    this.route.paramMap.subscribe((paramMap) => {
+      this.filmId = paramMap.get('id');
+      const category = data.find((cat) => cat.categoryId === this.categoryId);
+      this.film = category?.list.find((film) => film.filmId === this.filmId);
+      window.scrollTo(0, document.body.scrollHeight);
+    });
+  }
 }
